7d99e79d7ec88c17cd7223c6dc38737353e04e41
tigran.tskanyan <tigran.tskanyan@spbtv.com>
2022-03-13 11:05:56 +0300

    add colors into select (doctors page)

--- frontend/static/js/doctors.js
+++ frontend/static/js/doctors.js
@@ -1,44 +1,55 @@
 window.addEventListener("DOMContentLoaded", () => {
   const select = document.querySelector("select");
   const pags = document.querySelectorAll(".pagination_bullet");
-  const instances = M.FormSelect.init(select, { transition: 0 });
-  const urlParams = new URLSearchParams(window.location.search);
+  const instances = M.FormSelect.init(select);
+  const url_params = new URLSearchParams(window.location.search);
+
+  console.log(window.colors);
+
+  instances.dropdownOptions.childNodes.forEach((el, index) => {
+    if (index === 0) return;
+    el.style.borderLeft = `8px solid ${window.colors[index - 1]}`;
+  });
+
+  const change_select = (e) => {
+    setTimeout(() => {
+      if (!window.location.href.includes("medicine_direction")) {
+        window.location.href = `?medicine_direction=${e.target.value}`;
+      } else {
+        const number = url_params.get("medicine_direction");
+        window.location.href = window.location.href.replace(
+          `medicine_direction=${number}`,
+          `medicine_direction=${e.target.value}`
+        );
+      }
+    }, 150);
+  };
+
+  const pags_init = (el) => {
+    el.addEventListener("click", (e) => {
+      if (!window.location.href.includes("page")) {
+        if (window.location.href.includes("?")) {
+          window.location.href =
+            window.location.href + `&page=${e.target.value}`;
+        } else {
+          window.location.href =
+            window.location.href + `?&page=${e.target.value}`;
+        }
+      } else {
+        const number = url_params.get("page");
+        window.location.href = window.location.href.replace(
+          `page=${number}`,
+          `page=${e.target.value}`
+        );
+      }
+    });
+  };
 
   if (instances) {
-    instances.el.addEventListener("change", (e) => {
-      setTimeout(() => {
-        if (!window.location.href.includes("medicine_direction")) {
-          window.location.href = `?medicine_direction=${e.target.value}`;
-        } else {
-          const number = urlParams.get("medicine_direction");
-          window.location.href = window.location.href.replace(
-            `medicine_direction=${number}`,
-            `medicine_direction=${e.target.value}`
-          );
-        }
-      }, 150);
-    });
+    instances.el.addEventListener("change", change_select);
   }
 
   if (pags.length) {
-    pags.forEach((el) => {
-      el.addEventListener("click", (e) => {
-        if (!window.location.href.includes("page")) {
-          if (window.location.href.includes("?")) {
-            window.location.href =
-              window.location.href + `&page=${e.target.value}`;
-          } else {
-            window.location.href =
-              window.location.href + `?&page=${e.target.value}`;
-          }
-        } else {
-          const number = urlParams.get("page");
-          window.location.href = window.location.href.replace(
-            `page=${number}`,
-            `page=${e.target.value}`
-          );
-        }
-      });
-    });
+    pags.forEach(pags_init);
   }
 });
